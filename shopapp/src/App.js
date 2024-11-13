bc638609b144b914c42995d48d5bb89ecfe53a6c
Banuprakash <banuprakashc@yahoo.co.in>
2024-11-13 12:11:40 +0530

    shopapp

--- shopapp/src/App.js
+++ shopapp/src/App.js
@@ -1,11 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import NavbarComponent from './components/NavbarComponent';
+import {Routes, Route} from 'react-router-dom'
+import ProductList from './components/ProductList'
+import ProductForm from './components/ProductForm'
+import Cart from './components/Cart'
+import Details from './components/Details'
+import Default from './components/Default'
 
 function App() {
   return (
     <div>
       <NavbarComponent />
+      <Routes>
+        <Route path='/products' element={<ProductList />} />
+        <Route path='/new_product' element={<ProductForm />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/details/:id' element={<Details />} />
+        <Route path='/' element={<ProductList />} />
+        <Route path='*' element={<Default />} />
+      </Routes>
     </div>
   );
 }
