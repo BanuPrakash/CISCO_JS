6aa75eab3053905a5e2cfb3d65bf8d4f6a5d83dd
Banuprakash <banuprakashc@yahoo.co.in>
2024-11-13 10:58:36 +0530

    day 3

--- hooksexample/src/context/Third.jsx
+++ hooksexample/src/context/Third.jsx
@@ -2,12 +2,25 @@ import React, { useContext } from 'react'
 import { UserContext } from './UserContextProvider'
 
 export default function Third() {
-    let {avatar, name} = useContext(UserContext); //Consumer
-  return (
-    <div>
-        Third <br />
-        Avatar: {avatar} <br />
-        Name: {name}
-    </div>
-  )
+    //     let {avatar, name} = useContext(UserContext); //Consumer
+    //   return (
+    //     <div>
+    //         Third <br />
+    //         Avatar: {avatar} <br />
+    //         Name: {name}
+
+    //     </div>
+    //   )
+
+    return (
+        <UserContext.Consumer>
+            {
+                value => <div>
+                    Name : {value.name} <br />
+                    Avatar: {value.avatar}
+                </div>
+
+            }
+        </UserContext.Consumer>
+    )
 }
