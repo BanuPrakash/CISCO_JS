aa25f65bfdbb88572554e0857ea8bc3f5bfb17b0
Banuprakash <banuprakashc@yahoo.co.in>
2024-11-13 14:32:40 +0530

    shopapp

--- shopapp/src/components/NavbarComponent.jsx
+++ shopapp/src/components/NavbarComponent.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Badge } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
+import { CartContext } from '../context/CartContextProvider';
 
 export default function NavbarComponent() {
+    let {quantity} = useContext(CartContext);
     return (
         <Navbar bg="dark" data-bs-theme="dark">
             <Container>
                 <Navbar.Brand>ACME Shop</Navbar.Brand>
                 <Nav className="me-auto">
                     <Nav.Link as={Link} to="/">Products</Nav.Link>
-                    <Nav.Link as={Link} to="/cart">Cart</Nav.Link>
+                    <Nav.Link as={Link} to="/cart">Cart <Badge>{quantity}</Badge></Nav.Link>
                     <Nav.Link as={Link} to="/new_product">New Product</Nav.Link>
                 </Nav>
             </Container>
--- shopapp/src/components/ProductCard.jsx
+++ shopapp/src/components/ProductCard.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { CartContext } from '../context/CartContextProvider';
 
 export default function ProductCard({ product }) {
-    let {id, image, title, description, price} = product;
+    let { id, image, title, description, price } = product;
+    let { addToCart } = useContext(CartContext);
     return (
         <div className='col-md-4'>
             <Card style={{ width: '18rem' }}>
@@ -16,7 +18,15 @@ export default function ProductCard({ product }) {
                 </Card.Body>
                 <Card.Footer>
                     Rs. {price} &nbsp;
-                    <Button variant="primary">Add to Cart</Button>
+                    <Button variant="primary" onClick={() => addToCart({
+                        id,
+                        image,
+                        title,
+                        description,
+                        price,
+                        quantity: 1,
+                        amount: price
+                    })}>Add to Cart</Button>
                 </Card.Footer>
             </Card>
         </div>
