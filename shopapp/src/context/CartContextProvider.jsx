a6cb7cf6f67ecf30e0d504f7d9cbf2566c2d7582
Banuprakash <banuprakashc@yahoo.co.in>
2024-11-13 15:28:40 +0530

    shopapp

--- shopapp/src/context/CartContextProvider.jsx
+++ shopapp/src/context/CartContextProvider.jsx
@@ -1,5 +1,7 @@
 import React, { createContext, useReducer } from 'react'
 import cartReducer from '../reducers/cartReducer';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 export const CartContext = createContext(); // central placeholder for cart 
 
@@ -9,22 +11,38 @@ let initialState = {
     quantity: 0
 };
 
-export default function CartContextProvider({children}) {
+export default function CartContextProvider({ children }) {
+    let navigate = useNavigate(); // hook
     let [state, dispatch] = useReducer(cartReducer, initialState);
     function addToCart(item) {
-        dispatch({type: 'ADD_TO_CART', payload: item});
+        dispatch({ type: 'ADD_TO_CART', payload: item });
     }
 
     function increment(id) {
-        dispatch({type: 'INCREMENT', payload: id})
+        dispatch({ type: 'INCREMENT', payload: id })
     }
 
     function clearCart() {
-        dispatch({type: 'CLEAR_CART'})
+        // assume on login success user email is stored in sessionStorage
+        // window.sessionStorage.setItem("customer", "banu@gmail.com");
+
+        let order = {
+            customer: window.sessionStorage.getItem("customer"),
+            order_date: new Date(),
+            items: state.cartItems
+        };
+
+        axios.post("http://localhost:1234/orders", order).then(response => {
+            console.log("Order Placed!!")
+            dispatch({ type: 'CLEAR_CART' });
+            navigate('/');
+        })
+
+
     }
     return (
-       <CartContext.Provider value={{...state, addToCart, increment, clearCart}}>
+        <CartContext.Provider value={{ ...state, addToCart, increment, clearCart }}>
             {children}
-       </CartContext.Provider>
+        </CartContext.Provider>
     )
 }
